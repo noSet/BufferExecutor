5ed67c146d7a7b9570bdb380261bfcecb74c8e6d
noSet <342947998@qq.com>
2021-11-28 23:14:47 +0800

    init

---
+++ BufferExecutor.Test/BufferExecutorTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using Xunit;
+
+namespace BufferExecutor.Test;
+
+public class BufferExecutorTest
+{
+    [Theory]
+    [InlineData(1, false)]
+    [InlineData(2, true)]
+    public void ExecuteTest(int num, bool result)
+    {
+        var action = () =>
+        {
+            Thread.Sleep(200);
+            num--;
+        };
+
+        // 始终执行两次
+        BufferExecutor executor = new();
+        for (int i = 0; i < 10; i++)
+        {
+            Thread.Sleep(0); // 放弃当前线程时间片，使executor内部已经开始使用线程池线程
+            executor.Execute(action);
+        }
+
+        Thread.Sleep(2000);
+
+        Assert.Equal(num == 0, result);
+    }
+}
---
+++ BufferExecutor/BufferExecutor.cs
@@ -0,0 +1,47 @@
+﻿namespace BufferExecutor;
+public class BufferExecutor
+{
+    private int limit;
+    private Action? action;
+    private readonly object sync = new();
+
+    public void Execute(Action action)
+    {
+        lock (sync)
+        {
+            this.action = action;
+
+            if (limit == 0)
+            {
+                limit++;
+                ExecuteCore();
+            }
+        }
+
+    }
+
+    private void ExecuteCore()
+    {
+        ThreadPool.QueueUserWorkItem(_ =>
+        {
+            Action? action = null;
+
+            while (true)
+            {
+                lock (sync)
+                {
+                    if (this.action == null)
+                    {
+                        limit--;
+                        break;
+                    }
+
+                    action = this.action;
+                    this.action = null;
+                }
+
+                action();
+            }
+        });
+    }
+}
